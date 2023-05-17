5451bd06110ce4e1060d722ec045a90fb52c0fb8
Jatin Patel <jatin.p@amarinfotech.com>
2023-05-17 12:13:59 +0530

    implement invoice product add/remoe/show demo

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,172 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+// import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {},
+  props: {},
+  data() {
+    return {
+      totalProductCount: 0,
+      categories: [
+        {
+          name: "1",
+          display: "Category 1",
+        },
+        {
+          name: "2",
+          display: "Category 2",
+        },
+        {
+          name: "3",
+          display: "Category 3",
+        },
+      ],
+      selectedCategory: 1,
+      invProducts: [
+        {
+          product_name: "test1",
+          category: "1",
+          price: 100,
+          discount: 5,
+          total: 95,
+        },
+      ],
+      invProdForDisplay: [],
+    };
+  },
+  methods: {
+    addProducts() {
+      const tempProduct = {
+        product_name: "",
+        category: "",
+        price: null,
+        discount: null,
+        total: null,
+      };
+      this.totalProductCount += 1;
+      this.invProducts.push(tempProduct);
+    },
+    removeProduct(index) {
+      this.invProducts.splice(0, index);
+    },
+    generateTable() {
+      let tempInvProd = this.invProducts.map((invProd) => {
+        let catDetail = this.categories.filter((cat) => {
+          return cat.name == invProd.category;
+        });
+        console.log(catDetail);
+        // invProd.category = catDetail[0].display;
+        return invProd;
+      });
+
+      this.invProdForDisplay = tempInvProd;
+    },
+    calculateDiscount(product) {
+      console.log(product);
+    },
+  },
+};
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="You did it!" /> -->
+      <div class="invoice-head">
+        <div class="add-more">
+          <button @click="addProducts()">Add Product</button>
+        </div>
+        <div
+          class="inv-prod"
+          v-for="(product, index) of invProducts"
+          :key="index"
+        >
+          <div class="product-name inv-pading inv-border">
+            <input type="text" :value="product.product_name" />
+          </div>
+          <div class="product-category inv-pading inv-border">
+            <select>
+              <option
+                v-for="(category, index) in categories"
+                :key="index"
+                :value="category.name"
+              >
+                {{ category.display }}
+              </option>
+            </select>
+          </div>
+          <div class="product-price inv-pading inv-border">
+            <input
+              type="number"
+              :value="product.price"
+              @keyup="calculateDiscount(product)"
+            />
+          </div>
+          <div class="product-discount inv-pading inv-border">
+            <input type="number" :value="product.discount" />
+          </div>
+          <div class="product-total inv-pading inv-border">
+            <input type="number" :value="product.total" />
+          </div>
+          <div class="remove-product inv-pading inv-border" style="width: 32px">
+            <div v-if="index != 0">
+              <button @click="removeProduct(index)" style="font-weight: bold">
+                -
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="generate">
+          <button @click="generateTable()">Generate Table</button>
+        </div>
+      </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <div class="invoice-table">
+        <div class="inv-prod-head">
+          <div class="tbl-prod-name-head inv-head-pading">Product Name</div>
+          <div class="tbl-prod-category-head inv-head-pading">
+            Product Category
+          </div>
+          <div class="tbl-prod-price-head inv-head-pading">Product Price</div>
+          <div class="tbl-prod-disc-head inv-head-pading">Product Discount</div>
+          <div class="tbl-prod-total-head inv-head-pading">Product Total</div>
+        </div>
+        <div
+          class="inv-prod-detail"
+          v-for="(prod, index) in invProdForDisplay"
+          :key="index"
+        >
+          <div class="tbl-prod-name inv-prod-detail-pading">
+            {{ prod.product_name }}
+          </div>
+          <div class="tbl-prod-category inv-prod-detail-pading">
+            {{ prod.category }}
+          </div>
+          <div class="tbl-prod-price inv-prod-detail-pading">
+            {{ prod.price }}
+          </div>
+          <div class="tbl-prod-disc inv-prod-detail-pading">
+            {{ prod.discount }}
+          </div>
+          <div class="tbl-prod-total inv-prod-detail-pading">
+            {{ prod.total }}
+          </div>
+        </div>
+      </div>
     </div>
   </header>
 
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
@@ -31,55 +180,31 @@ header {
   margin: 0 auto 2rem;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
+.inv-prod {
+  display: flex;
+}
+
+.inv-pading {
+  padding: 10px;
+}
+.inv-border {
+  border: 1px solid #ccc;
+}
+.inv-prod-head {
+  display: flex;
+}
+.inv-head-pading {
+  padding: 10px;
+  /* font-weight: bold; */
+  border: 1px solid #ccc;
+}
+.inv-prod-detail {
+  display: flex;
+}
+.inv-prod-detail-pading {
+  padding: 10px;
+  width: 6rem;
   text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  border: 1px solid #ccc;
 }
 </style>
